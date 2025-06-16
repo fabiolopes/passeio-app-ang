e0280a3a43132b48f9bd42daa0dc37c5ab3dc795
unknown <fabiolopesbione@hotmail.com>
2025-06-15 22:44:46 -0300

    aplicando titulo e subtitulo por rota ativada

--- src/app/template/template-routing.module.ts
+++ src/app/template/template-routing.module.ts
@@ -10,17 +10,29 @@ const routes: Routes = [
       {
         path: 'categorias',
         loadChildren: () => import('../categorias/categorias.module').then(m => m.CategoriasModule),
-        pathMatch: 'full'
+        pathMatch: 'full',
+        data: {
+          titulo: 'Categorias',
+          subtitulo: 'Realize o cadastro de novas categorias'
+        }
       },
       {
         path: 'lugares',
         loadChildren: () => import('../lugares/lugares.module').then(m => m.LugaresModule),
-        pathMatch: 'full'
+        pathMatch: 'full',
+        data: {
+          titulo: 'Lugares',
+          subtitulo: 'Realize o cadastro de novos lugares'
+        }
       },
       {
         path: 'galeria',
         loadChildren: () => import('../galeria/galeria.module').then(m => m.GaleriaModule),
-        pathMatch: 'full'
+        pathMatch: 'full',
+        data: {
+          titulo: 'Galeria',
+          subtitulo:'Descubra os melhores lugares para explorar e se divertir'
+        }
       }
     ]
    }
